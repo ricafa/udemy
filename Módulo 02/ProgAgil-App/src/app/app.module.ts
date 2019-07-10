6efe0f95a8fe9c8410bfe13320a80de925ba9b47
Ricardo Camargo Favaro <ricardo-favaro@uam.com.br>
2019-07-10 13:23:27 -0300

    aula 31 finished

--- Módulo 02/ProgAgil-App/src/app/app.module.ts
+++ Módulo 02/ProgAgil-App/src/app/app.module.ts
@@ -4,18 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EventosComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      EventosComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
